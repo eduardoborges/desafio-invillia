852fb02d8b321deb72dc56146c278a8e50310309
Eduardo Borges <euduardoborges@gmail.com>
2019-05-24 20:54:56 -0300

    Adicionado Bulma estaticamente por hora

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as sw from "./sw";
 
+import "bulma/css/bulma.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 sw.unregister();
