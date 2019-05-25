0e47c2b7f5690482f236c26e3f475384a16c369d
Eduardo Borges <euduardoborges@gmail.com>
2019-05-24 21:56:43 -0300

    Base dos servicinhossss

---
+++ src/services/people.js
@@ -0,0 +1,31 @@
+import api from "./index";
+
+/**
+ * Get all entries
+ */
+const index = async () => {
+  console.log("Indexando bb");
+};
+
+/**
+ * Crate an entry
+ */
+const create = async () => {
+  console.log("Criando bb");
+};
+
+/**
+ * Read an entry
+ */
+const read = async () => {
+  console.log("Lendo bb");
+};
+
+/**
+ * Update an entry
+ */
+const update = async () => {
+  console.log("Atualizando bb");
+};
+
+export { index, create, read, update };
