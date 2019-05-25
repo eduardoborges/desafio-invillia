7f19fedff96577fbdb91b43c21d8adcb3a056a14
Eduardo Borges <euduardoborges@gmail.com>
2019-05-25 17:11:12 -0300

    Adicao de logica de resolucao de Starships

--- src/components/People/index.jsx
+++ src/components/People/index.jsx
@@ -1,88 +1,84 @@
 // @flow
 
-import React, { useState, useEffect } from 'react';
-import { StarShips } from '../index';
-import Axios from 'axios';
+import React, { useState, useEffect, Fragment } from "react";
+import { StarShips } from "../index";
+import Axios from "axios";
 
 type PeopleProps = {
-	name: String,
-	height: Number,
-	mass: Number,
-	hair_color: String,
-	skin_color: String,
-	eye_color: String,
-	birth_year: String,
-	gender: String,
-	homeworld: String,
-	films: Array<String>,
-	species: Array<String>,
-	vehicles: Array<String>,
-	starships: Array<String>,
-	created: Date,
-	edited: Date,
-	url: String
+  name: String,
+  height: Number,
+  mass: Number,
+  hair_color: String,
+  skin_color: String,
+  eye_color: String,
+  birth_year: String,
+  gender: String,
+  homeworld: String,
+  films: Array<String>,
+  species: Array<String>,
+  vehicles: Array<String>,
+  starships: Array<String>,
+  created: Date,
+  edited: Date,
+  url: String
 };
 
-export default
-	function People(props: PeopleProps) {
+export default function People(props: PeopleProps) {
+  const [starShipsModal, setStarshipsModal] = useState(false);
+  const [starshipsData, setStarShipsData] = useState([]);
 
-	const [imageUrl, setImageUrl] = useState(false);
+  useEffect(() => {
+    resolveStarships();
+  }, []);
 
-	useEffect(() => {
-		resolveImage();
-	}, [])
+  const resolveStarships = async () => {
+    let array = []
+    props.starships.forEach(async url => {
+      const { data } = await Axios.get(url);
+      array.push(data);
+    });
+    setStarShipsData(array);
+  }
+
+  return (
+    <Fragment>
+
+      {/* modals */}
+      {starShipsModal && <StarShips data={starshipsData} handleClose={() => setStarshipsModal(false)} />}
 
 
-	const resolveImage = async () => {
-		const { data } = await Axios.get('https://api.qwant.com/api/search/images', {
-			params: {
-				'count': 50,
-				'q': "toulouse pink city",
-				't': 'images',
-				'safesearch': 1,
-				'locale': 'en_US',
-				'uiv': 4
-			}
-		});
-		setImageUrl(data.result.items[0].thumbnail);
-	}
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={"https://bulma.io/images/placeholders/1280x960.png"}
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{props.name}</p>
+              <p className="subtitle is-6" style={{ textTransform: "capitalize" }}>
+                {(props.height * 0.01).toFixed(2)}m | {props.mass}kgs | {props.hair_color} hair |{" "}
+                {props.eye_color} eyes
+              </p>
+            </div>
+          </div>
 
-	return (
-		<div class="card">
-			<div class="card-image">
-				<figure class="image is-4by3">
-					<img
-						src={imageUrl ? imageUrl : "https://bulma.io/images/placeholders/1280x960.png"}
-						alt="Placeholder image"
-					/>
-				</figure>
-			</div>
-			<div class="card-content">
-				<div class="media">
-					<div class="media-left">
-						<figure class="image is-48x48">
-							<img
-								src="https://bulma.io/images/placeholders/96x96.png"
-								alt="Placeholder image"
-							/>
-						</figure>
-					</div>
-					<div class="media-content">
-						<p class="title is-4">{props.name}</p>
-						<p class="subtitle is-6">
-							{props.height * 0.01}m | {props.mass}kgs | borns on {props.birth_year}
-						</p>
-					</div>
-				</div>
+          <div className="content">
+            <time dateTime={props.birth_year} style={{ textTransform: "capitalize" }}>
+              borns on {props.birth_year}
+            </time>
+            <hr />
+            <button className="button is-primary is-fullwidth" disabled={props.starships.length === 0} onClick={() => setStarshipsModal(true)}>
+              View Starships ({props.starships.length})
+              </button>
 
-				<div class="content">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
-					mauris. <a>@bulmaio</a>.
-					<a href="#">#css</a> <a href="#">#responsive</a>
-					<br />
-					<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-				</div>
-			</div>
-		</div>
-	);
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
