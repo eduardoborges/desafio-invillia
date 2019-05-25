a2a0261396d7492ad8115ebf0ea70b4538a99272
Eduardo Borges <euduardoborges@gmail.com>
2019-05-25 15:38:36 -0300

    As telas agora sao Classes

--- src/screens/search/index.js
+++ src/screens/search/index.js
@@ -1,62 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'unistore/react';
+import React, { useEffect, useState } from "react";
+import { connect } from "unistore/react";
+import { searchInListByField } from "../../utils";
 
-import searchActions from '../../actions/searchActions';
-import { searchInListByField } from '../../utils';
+// acontions
+import searchActions from "../../actions/searchActions";
 
-import { People } from '../../components';
+// components
+import { People } from "../../components";
 
-function SearchScreen(props) {
-	//
+class SearchScreen extends React.Component {
+  state = {
+    filter: ""
+  };
 
-	const { PEOPLE, getPeople } = props;
+  componentWillMount = () => {
+    this.props.getPeople();
+    window.addEventListener("scroll", this.handleScroll);
+  };
 
-	const [filter, setFilter] = useState('');
-	const deps = [];
-	useEffect(() => {
-		getPeople();
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
+  componentWillUnmount = () => {
+    window.removeEventListener("scroll", this.handleScroll);
+  };
 
-	const handleScroll = () => {
-		const { offsetHeight, scrollTop } = document.documentElement;
-		const offset = 100; // um pouquinho antes né bb
-		if (window.innerHeight + scrollTop <= offsetHeight - offset) return;
-		getPeople();
-	};
+  handleScroll = () => {
+    const { offsetHeight, scrollTop } = document.documentElement;
+    const offset = 100; // um pouquinho antes né bb
+    if (window.innerHeight + scrollTop <= offsetHeight - offset) return;
+    this.props.getPeople();
+  };
 
-	return (
-		<div className="container">
-			<div className="columns">
-				<div className="column">
-					<h1 className="title">Search Screen</h1>
-				</div>
-				<div className="column">
-					<input
-						type="text"
-						className="input is-medium"
-						placeholder="Filter by name"
-						onChange={e => setFilter(e.target.value)}
-					/>
-				</div>
-			</div>
+  render = () => {
+    const { PEOPLE } = this.props;
+    const { filter } = this.state;
 
-			<hr />
+    return (
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h1 className="title">Search Screen</h1>
+          </div>
+          <div className="column">
+            <input
+              type="text"
+              className="input is-medium"
+              placeholder="Filter by name"
+              onChange={e => this.setState({ filter: e.target.value })}
+            />
+          </div>
+        </div>
 
-			<div className="peoples columns is-multiline">
-				{searchInListByField(PEOPLE.data, 'name', filter).map(item => (
-					<div className="column is-one-quarter">
-						<People {...item} />
-					</div>
-				))}
-			</div>
+        <hr />
 
-			<br />
+        <div className="peoples columns is-multiline">
+          {searchInListByField(PEOPLE.data, "name", filter).map((item, index) => (
+            <div className="column is-one-quarter">
+              <People {...item} />
+            </div>
+          ))}
+        </div>
 
-			{PEOPLE.isLoading && <div className="sub-title">Carregando...</div>}
-		</div>
-	);
+        <br />
+
+        {PEOPLE.isLoading && <div className="sub-title">Carregando...</div>}
+      </div>
+    );
+  };
 }
 
-export default connect('PEOPLE', searchActions)(SearchScreen);
+export default connect(
+  "PEOPLE",
+  searchActions
+)(SearchScreen);
